5d137346826b30d8ba5b3cc1bdff2c77aef1719f
Jam <jam29n@gmail.com>
2024-12-04 15:21:40 +0100

    add token

--- create-order.js
+++ create-order.js
@@ -5,6 +5,32 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+ 
+
+async function getAccessToken() {
+  const response = await fetch("https://demo-accounts.vivapayments.com/connect/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      grant_type: "client_credentials",
+      client_id: "69upvpezosc0e06egvbwjwh19qgcmyd3y9wembj0cn260.apps.vivapayments.com", // Replace with your Client ID
+      client_secret: "MzGSKzc3PRTi881s1Q2rewV086u7wG", // Replace with your Client Secret
+    }),
+  });
+
+  const data = await response.json();
+
+  if (response.ok) {
+    console.log("Access Token:", data.access_token);
+    return data.access_token;
+  } else {
+    throw new Error(data.error_description || "Failed to get access token.");
+  }
+}
+
+let monToken = getAccessToken()
 
 // Route to create an order
 app.post("/create-order", async (req, res) => {
@@ -20,7 +46,7 @@ app.post("/create-order", async (req, res) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer <YOUR_ACCESS_TOKEN>`, // Replace with your Viva Wallet token
+        Authorization: `Bearer ${monToken}`, // Replace with your Viva Wallet token
       },
       body: JSON.stringify({
         amount: amount, // Amount in cents
